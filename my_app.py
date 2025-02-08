80b167a9c1ef4e55749732d62906bc4f4a15b396
Bejanyan04 <bejanyananna2000@gmail.com>
2025-02-08 18:54:35 +0400

    changes

--- my_app.py
+++ my_app.py
@@ -8,9 +8,17 @@ model = torch.load("best_model")
 
 @app.post("/predict")
 async def predict(input_data: list):
+"""
+    classes = load_mlb_mapping()
+  test_data = get_test_data()
+  tokenizer = get_tokenizer()
+  model = get_model() #get best model( finetuned huggingface model)
+  
     result = model(torch.tensor(input_data)).tolist()
     return {"prediction": result}
 
+"""
+pass
 
 if __name__ == "__main__":
     import uvicorn
